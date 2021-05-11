b4d963697cb65769942415b16f37b340cb7ae00d
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2021-05-11 21:19:38 +0900

    update

--- main.js
+++ main.js
@@ -7,17 +7,32 @@ const { remote } = require("webdriverio");
         capabilities: {
             "platformName": "ios",
             "appium:automationName": "XCUITest",
-            "appium:platformVersion": "14.4",
+            "appium:platformVersion": "14.5",
             "appium:deviceName": "iPhone Simulator",
-            "appium:app": "./example.app.zip",
+            "appium:app": "./KickDebug.app.zip",
         }
     });
 
-    const element = await client.findElement(
+    let element1 = await client.findElement(
         "xpath",
         '//XCUIElementTypeButton[@name="Log in"]'
     )
-    await client.elementClick(element["element-6066-11e4-a52e-4f735466cecf"])
-    await client.saveScreenshot("./tmp/screenshot.png");
+    await client.elementClick(element1["element-6066-11e4-a52e-4f735466cecf"])
+    await client.saveScreenshot("./tmp/1-screenshot.png");
+
+    const element2 = await client.findElement(
+        "xpath",
+        '//XCUIElementTypeStaticText[@name=\"Log in\"]'
+    )
+    await client.elementClick(element2["element-6066-11e4-a52e-4f735466cecf"])
+    await client.saveScreenshot("./tmp/2-screenshot.png");
+
+    const element3 = await client.findElement(
+        "xpath",
+        "//XCUIElementTypeApplication[@name=\"KickDebug\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeTextField"
+    )
+    await client.elementClick(element3["element-6066-11e4-a52e-4f735466cecf"])
+    await client.saveScreenshot("./tmp/3-screenshot.png");
+
     await client.deleteSession();
 })();
